9e167dbb246fafc66a0520d3735df9da79cb993e
l-burrell <eewinterz@gmail.com>
2023-03-28 18:22:23 -0400

    removed unnecessary duplicates

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 
 def index():
     return render_template('index.html')
+
 
 @app.route('/register')
 
 def register():
     return render_template('register.html')
 
+
 @app.route('/PostersToJudge')
 
 def PostersToJudge():
     return render_template('PostersToJudge.html')
 
-@app.route('/')
+
+@app.route('/upload_poster')
 def Upload_poster():
     return render_template('Upload_poster.html')
+
 
 @app.route('/submit_poster', methods=['POST'])
 def submit_poster():
@@ -28,5 +33,7 @@ def submit_poster():
     # do something with the file
     return 'File uploaded successfully!'
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
