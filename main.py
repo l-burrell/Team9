cc27e3eb7cbcc0b3d8bf26b148a22e5968668fe7
fchadni1 <60414303+fchadni1@users.noreply.github.com>
2023-03-28 00:07:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,3 +16,17 @@ def register():
 
 def PostersToJudge():
     return render_template('PostersToJudge.html')
+
+@app.route('/')
+def Upload_poster():
+    return render_template('Upload_poster.html')
+
+@app.route('/submit_poster', methods=['POST'])
+def submit_poster():
+    file = request.files['image']
+    file.save('/path/to/save/folder/' + file.filename)
+    # do something with the file
+    return 'File uploaded successfully!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
