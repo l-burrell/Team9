39a077e462c66a9b88a0393844a44d7bf08682ad
l-burrell <eewinterz@gmail.com>
2023-04-04 17:40:14 -0400

    updating the login page and changing directory

--- main.py
+++ main.py
@@ -4,33 +4,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
 
 @app.route('/register')
-
 def register():
     return render_template('register.html')
 
 
-@app.route('/PostersToJudge')
-
+@app.route('/posters')
 def PostersToJudge():
-    return render_template('PostersToJudge.html')
+    return render_template('posters.html')
 
 
 @app.route('/upload_poster')
 def Upload_poster():
-    return render_template('Upload_poster.html')
+    return render_template('upload_poster.html')
 
 
 @app.route('/submit_poster', methods=['POST'])
 def submit_poster():
     file = request.files['image']
     file.save('/path/to/save/folder/' + file.filename)
-    # do something with the file
     return 'File uploaded successfully!'
 
 
