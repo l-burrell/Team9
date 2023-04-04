8afbf9588aae02fd9389db18b56484aa74ae32f4
l-burrell <eewinterz@gmail.com>
2023-04-04 17:40:14 -0400

    creating the database

--- static/login.js
+++ static/login.js
@@ -8,10 +8,12 @@ btn.addEventListener('click', () => {
     var email = document.getElementById('email').value;
     console.log("email: ", email)
     if(re_jud.test(email)){
-        self.window.location.href = "posterstojudge.html";
+        self.window.location.href = "posters";
+        // self.window.location.href = "posterstojudge";
     }
     else if(re_con.test(email)){
-        self.window.location.href = "upload_poster.html";
+        self.window.location.href = "upload_poster";
+        // self.window.location.href = "upload_poster";
     }
     else {
         console.log('[invalid email] must be a gsu associated email address');
